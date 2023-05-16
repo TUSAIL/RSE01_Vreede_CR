f070e3411ae2ede0f480c73b34fb6deb190f5dc6
Luisa Orozco <l.orozco@esciencecenter.nl>
2023-05-16 12:30:15 +0200

    Add mean_temperature and get_spreadsheet_colums functions with docstrings

--- conversion.py
+++ conversion.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def celsius_to_kelvin(celsius):
     """
     Function to return Kelvin degrees from Celsius input
@@ -9,3 +11,32 @@ def kelvin_to_celsius(kelvin):
 
 def celsius_to_fahrenheit(celsius):
     return celsius * 9./5 + 32
+
+def mean_temperature(data):
+    """
+    Get the mean temperature.
+
+    Args:
+        data (pandas.DataFrame): A pandas dataframe with air temperature measurements.
+
+    Returns:
+        The mean air temperature (float)
+    """
+    temperatures = data['Air temperature (degC)']
+    return sum(temperatures)/len(temperatures)
+
+def get_spreadsheet_columns(file_loc, print_columns=False):
+    """Gets and prints the spreadsheet's header columns
+    
+    Args:
+       file_loc (str): The file location of the spreadsheet
+       print_columns (bool, optional) : A flag used to print the columns to the console (default is False)
+
+    Returns:
+       a list of strings used that are the header columns
+    """
+    file_data = pd.read_excel(file_loc)
+    column_headers = list(file_data.columns.values)
+    if print_columns:
+        print("\n".join(column_headers))
+    return column_headers
